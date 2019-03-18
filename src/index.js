4d2642df29d770faa75206a7cdecbe191372cf26
Kevin Yeh <revinyeh@gmail.com>
2019-03-18 09:40:44 -0700

    all of the old version before refector to hooks version

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			latitude: null,
+			errorMessage: ''
+		}
+	}
+
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			position => this.setState({ latitude: position.coords.latitude }),
+			err => this.setState({ errorMessage: err.message })
+		)
+	}
+
+	renderContent() {
+		if(this.state.errorMessage && !this.state.latitude) {
+			return <div>Error: {this.state.errorMessage}</div>
+		}
+		if(!this.state.erroeMessage && this.state.latitude) {
+			return <SeasonDisplay lat={this.state.latitude} />
+		}
+		return <Spinner message="Please accept location request!"/>
+	}
+
+	render() {
+		return(
+			<div>
+				{this.renderContent()}
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
